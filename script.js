1c353e3240ea805c74ba5c4204a1f2627aa670c0
pacokui <pacokui@github.com>
2024-11-08 16:47:05 +0100

    Made computer choice with random number

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+//console.log("Hello World!")
+
+function getComputerChoice(){
+   const randomNumber = Math.random();
+   if (randomNumber < 0.33){
+    return "Rock";
+   } else if (randomNumber < 0.66) {
+    return "Scissors";
+   }
+   else {
+    return "Paper";
+   }
+    
+}
+console.log(getComputerChoice());
