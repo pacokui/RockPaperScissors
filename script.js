6df21aa11603a1dce0a55c02743acd20eaf43408
pacokui <pacokui@github.com>
2024-11-08 18:31:41 +0100

    Made the game with for loop for 5 rounds

--- script.js
+++ script.js
@@ -12,18 +12,20 @@ function getComputerChoice(){
    }
     
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 function getHumanChoice(){
     let choice = prompt("Choose: Rock, Paper, Scissors?");
+    return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
 
 }
-console.log(getHumanChoice())
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(){
+    
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
+    function playRound(humanChoice, computerChoice){
     if (humanChoice === computerChoice){
         return "It's a tie, another round!";
     } else if (
@@ -31,8 +33,42 @@ function playRound(humanChoice, computerChoice){
         (humanChoice === "paper" && computerChoice === "Rock") ||
         (humanChoice === "scissors" && computerChoice === "Paper") 
     ) {
+        
         return "You win!";
     } else {
+        
         return "You lose!";
     } 
 }
+for ( let i = 0; i < 5; i++) {
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    console.log(`Round ${i + 1}:`);
+    console.log(`You chose ${humanSelection}`);
+    console.log(`Computer chose ${computerSelection}`);
+
+    const roundResult = playRound(humanSelection, computerSelection);
+    console.log(roundResult);
+
+    if (roundResult === "You win!") {
+        humanScore++;
+    } else if (roundResult == "You lose!") {
+        computerScore++;
+    }
+    console.log(`Current score: You ${humanScore}, Computer : ${computerScore}`);
+
+}
+console.log("Game over!");
+if (humanScore > computerScore) {
+    console.log("You are the winner!");
+
+} else if (computerScore > humanScore) {
+    console.log("Computer is the winner")
+} else {
+    console.log("It's a tie")
+}
+
+}
+playGame();
